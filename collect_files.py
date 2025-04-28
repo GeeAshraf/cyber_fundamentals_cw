6d4659c811854afa2e2578f5c668da3a120ad596
GeeAshraf <gh2301237@tkh.edu.eg>
2025-04-28 09:31:38 +0300

    Update collect_files.py

--- collect_files.py
+++ collect_files.py
@@ -12,13 +12,12 @@ def collect_files(source_dir, destination_dir, extensions):
                 dest_path = os.path.join(destination_dir, filename)
                 shutil.copy2(source_path, dest_path)
 
-    print(f"Files with {extensions} copied to: {destination_dir}")
+    print(f"files with (extensions) copied to: {destination_dir}")
 
 def main():
-    source_dir = input("Enter the source directory (where files are located): ").strip()
+    source_dir = input("Enter the source directory (where files are located): ");
     destination_dir = "collected_files"
-    extensions_input = input("Enter file extensions to collect (comma separated, e.g., .txt,.docx,.jpg): ").strip()
-
+    extensions_input = input("Enter file extensions to collect (comma separated, e.g., .txt,.docx,.jpg): ");
     extensions = tuple(ext.strip().lower() for ext in extensions_input.split(","))
 
     collect_files(source_dir, destination_dir, extensions)
